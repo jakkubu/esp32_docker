571ebaf19f1ab4327ce67d170795d641d641e9f9
jakkubu <jakkubu@gmail.com>
2019-11-28 12:10:59 +0100

    modify clone

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -d $IDF_PATH ]; then
 	echo "$IDF_PATH - doesn't exists - creating"
 	cd $IDF_PATH/..
 	echo $IDF_PATH "cloning esp-idf repository to $IDF_PATH"
-	git clone -b v3.3 --recursive https://github.com/espressif/esp-idf.git
+	git clone -b v3.3 --recursive https://github.com/espressif/esp-idf.git esp-idf
 	cp -f $IDF_PATH/requirements.txt /config
 	echo "After copying esp-idf to working dir you should rebuild docker file with build argument IDF_REBUILD e.g.:
 		docker build -t=esp32 --build-arg IDF_REBUILD=$RANDOM ."
